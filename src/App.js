c863eb6eeafdb3a0ae2958ec36fc4a428ba3ae43
Ashley Thomas <115642737+ashleyth0mas@users.noreply.github.com>
2023-06-10 13:35:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Forecast from './component/Forecast';
 import getWeatherdata from './services/weatherServices';
 import { useState } from 'react';
 import { useEffect } from 'react';
-const bgimg='https://blog.depositphotos.com/wp-content/uploads/2017/07/Soothing-nature-backgrounds-2.jpg.webp'
+const bgimg='https://cdn.wallpaperhub.app/cloudcache/c/6/e/1/c/0/c6e1c0a6675b2b2b3f49ace94683d018a506bb11.jpg'
 function App() {
   const [query, setQuery] = useState({q: "kochi"});
   const [units , setUnits] = useState("metric");
@@ -29,7 +29,7 @@ useEffect (() => {
     <div classname="relative">
   <img  classname='w-full shadow-lg shadow-black object-cover
        rounded-lg mb-3' src={bgimg}></img>
-<div className=" absolute left-5  pt-4 right-5 top-5 bottom-15  mx-auto max-w-screen-md mt-4 py-5 px-32 bg-transparent shadow-xl shadow-gray-400 h-fit ${formatBackground()} ">
+<div className=" absolute left-5  pt-4 right-5 top-1 bottom-15  mx-auto max-w-screen-md mt-4 py-5 px-32 bg-transparent shadow-2xl shadow-gray-400 h-fit rounded-3xl ">
     
      <Topbuttons setQuery={setQuery} />
      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
